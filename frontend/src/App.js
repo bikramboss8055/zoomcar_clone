96f5fd273cd2ab2dc0156cf171cb0c9cf742e2ff
arpitkumarsahu <aarpitkumarsahu99@gmail.com>
2023-01-18 13:51:37 +0530

    DateRange Page Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-// import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <>
+    <div>
     
-    
-    </>
+    {/* <Navbar/> */}
+    {/* <Login/> */}
+    {/* <DatePicker/> */}
+    </div>
   );
 }
 
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <Box>
+        Login
+      </Box>
+    </div>
+  )
+}
+
+export default Login
