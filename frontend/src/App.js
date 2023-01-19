ad611e3ddc74b5210a4522a047fa5e8fc1f497f2
Abhra11 <abhra001597@gmail.com>
2023-01-20 00:59:05 +0530

    day 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+// import HomePage from "./pages/HomePage";
+import MyAccount from "./pages/MyAccount";
 
 function App() {
   return (
     <div>
-    
-    {/* <Navbar/> */}
-    {/* <Login/> */}
-    {/* <DatePicker/> */}
+      {/* <Navbar/> */}
+      {/* <Login/> */}
+      {/* <DatePicker/> */}
+      <MyAccount />
     </div>
   );
 }
