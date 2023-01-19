84fa050249d8e2b55cbe3ccb2242e72fc16b42db
arpitkumarsahu <aarpitkumarsahu99@gmail.com>
2023-01-19 17:30:44 +0530

    Day_2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
+import DatePicker from "./components/DatePicker";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div>
     
-    {/* <Navbar/> */}
-    {/* <Login/> */}
+    <Navbar/>
+    <Login/>
     {/* <DatePicker/> */}
+    {/* <HomePage/> */}
     </div>
   );
 }
