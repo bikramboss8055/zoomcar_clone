a5b8806873418f7974c1e8f6b1b913716e79e269
Abhra11 <abhra001597@gmail.com>
2023-01-23 00:27:56 +0530

    day 5

--- backend/middleware/VarifyToken.js
+++ backend/middleware/VarifyToken.js
@@ -1,30 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const secretKey = process.env.secretKey;
 
-const VarifyToken = async (req, res,next) =>{
+const VarifyToken = async (req, res, next) => {
   try {
     let token = req.headers.authtoken || null;
-    if(!token){
-        return res.status(401).send({msg : "Please Login First"})
+
+    if (!token) {
+      return res.status(401).send({ msg: "Please Login First" });
     }
 
-    jwt.verify(token, secretKey, (err, decode) =>{
-        if(err){
-            res
-            .status(500)
-            .send({ msg: "Invalid Token Please Login again", error });
-        }else{
-            let authId = decode._id
-            req.authId = authId;
-            next();
-        }
-    })
+    jwt.verify(token, secretKey, (err, decode) => {
+      if (err) {
+        res
+          .status(500)
+          .send({ msg: "Invalid Token Please Login again", error });
+      } else {
+        let authId = decode._id;
+        req.authId = authId;
+        next();
+      }
+    });
   } catch (error) {
-    res
-    .status(500)
-    .send({ msg: "Somthing Wrong with token", error });
+    res.status(500).send({ msg: "Somthing Wrong with token", error });
   }
-}
+};
 
-module.exports = {VarifyToken}
+module.exports = { VarifyToken };
